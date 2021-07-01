b5e4d15ae0e9bd8248c87c7785f484d30bb9ae9a
Sarthak Sarangi <77694787+sarthaksarangi@users.noreply.github.com>
2021-07-01 15:13:21 +0530

    Create insertEnd.java

---
+++ LinkedList/insertEnd.java
@@ -0,0 +1,42 @@
+class Node {
+    int data;
+    Node next;
+
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+}
+    class test {
+        public static Node insertEnd(Node head, int x) {
+            Node temp = new Node(x);
+            if (head==null)
+                return temp;
+          Node curr = head;
+          while(curr.next!=null) {
+              curr = curr.next;
+          }
+          curr.next= temp;
+          return head;
+
+
+    }
+        public static void main(String[] args) {
+            Node head= null;
+             head = insertEnd(head,10);
+            head = insertEnd(head,20);
+            head = insertEnd(head,30);
+            printlist(head);
+
+        }
+        public static void printlist(Node head){
+            Node curr= head;
+            while(curr!= null){
+                System.out.println(curr.data+ " ");
+                curr= curr.next;
+
+            }
+        }
+
+
+        }
