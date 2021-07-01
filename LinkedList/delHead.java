5ff27e70a9f071cf2a911e2ecda9c1453801ecdd
Sarthak Sarangi <77694787+sarthaksarangi@users.noreply.github.com>
2021-07-01 15:26:07 +0530

    Create delHead.java

---
+++ LinkedList/delHead.java
@@ -0,0 +1,35 @@
+class Node {
+    int data;
+    Node next;
+
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+}
+    class LinkedList{
+        public static void main(String[] args) {
+            Node head= new Node(10);
+            head.next = new Node (20);
+            head.next.next= new Node (30);
+            printlist(head);
+            head =delHead(head);
+            printlist(head);
+
+        }
+        static Node delHead(Node head){
+            if (head== null)
+                return null;
+            else
+                return head.next;
+
+        }
+        public static void printlist(Node head){
+            Node curr= head;
+            while (curr != null){
+                System.out.print(curr.data+" ");
+                curr= curr.next;
+            }
+            System.out.println();
+        }
+    }
