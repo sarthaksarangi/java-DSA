f21224552e7f7b0e27199150db6e959dd1decec1
Sarthak Sarangi <77694787+sarthaksarangi@users.noreply.github.com>
2022-02-04 22:59:33 +0530

    Create Rotated Binary Search.java

---
+++ DSA/Recursion/Rotated Binary Search.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class RotatedBinarySearch {
+    public static void main(String[] args) {
+int [] arr={5,6,7,8,1,2,3,4};
+        System.out.println(search(arr,2,0,arr.length-1));
+    }
+    static int search(int[]arr,int target,int s,int e){
+        int mid= s+(e-s)/2;
+        if(arr[mid]==target){
+            return mid;
+        }
+        if(arr[s]<=arr[mid]){
+            if(target>=arr[s]&&target<arr[mid]){
+                return search(arr,target,s,mid-1);
+            }
+            else return search(arr,target,mid+1,e);
+        }
+        if(target>arr[mid]&&target<=arr[e]){
+            return search(arr,target,mid+1,e);
+        }
+        else return search(arr,target,s,mid-1);
+    }
+
+}
