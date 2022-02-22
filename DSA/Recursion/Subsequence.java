f6bd1a3c9fc99c31cbf3af75913a25f3a07fa5cd
Sarthak Sarangi <77694787+sarthaksarangi@users.noreply.github.com>
2022-02-23 00:35:22 +0530

    Create Subsequence.java

---
+++ DSA/Recursion/Subsequence.java
@@ -0,0 +1,16 @@
+package com.company.Recursion;
+
+public class Subsequence {
+    public static void main(String[] args) {
+       subseq("","abc");
+    }
+    public static void subseq(String p, String up){
+        if(up.isEmpty()){
+            System.out.println(p);
+            return;
+        }
+        char ch = up.charAt(0);
+        subseq(p+ch,up.substring(1));
+        subseq(p,up.substring(1));
+    }
+}
