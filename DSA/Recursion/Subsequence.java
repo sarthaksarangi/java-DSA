e0e11921ce9670c47f3e8780bba9b84412b85ba8
Sarthak Sarangi <77694787+sarthaksarangi@users.noreply.github.com>
2022-02-23 23:04:02 +0530

    Update Subsequence.java

--- DSA/Recursion/Subsequence.java
+++ DSA/Recursion/Subsequence.java
@@ -1,8 +1,11 @@
 package com.company.Recursion;
 
+import java.util.ArrayList;
+
 public class Subsequence {
     public static void main(String[] args) {
-       subseq("","abc");
+//subseqAscii("","abc");
+        System.out.println(subseqListAscii("","abc"));
     }
     public static void subseq(String p, String up){
         if(up.isEmpty()){
@@ -12,5 +15,43 @@ public class Subsequence {
         char ch = up.charAt(0);
         subseq(p+ch,up.substring(1));
         subseq(p,up.substring(1));
+    }
+    public static ArrayList<String> subseqList(String p, String up){
+        if(up.isEmpty()){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        char ch = up.charAt(0);
+        ArrayList <String> left = subseqList(p+ch,up.substring(1));
+        ArrayList <String> right =  subseqList(p,up.substring(1));
+        left.addAll(right);
+        return left;
+    }
+    public static void subseqAscii(String p, String up){
+        if(up.isEmpty()){
+            System.out.println(p);
+            return;
+        }
+        char ch = up.charAt(0);
+        subseqAscii(p+ch,up.substring(1));
+        subseqAscii(p+(ch+0),up.substring(1));
+        subseqAscii(p,up.substring(1));
+
+    }
+    public static ArrayList<String> subseqListAscii(String p, String up){
+        if(up.isEmpty()){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        char ch = up.charAt(0);
+        ArrayList <String> first = subseqListAscii(p+ch,up.substring(1));
+        ArrayList <String> second =  subseqListAscii(p,up.substring(1));
+        ArrayList <String> third =  subseqListAscii(p+ (ch+0),up.substring(1));
+
+        first.addAll(second);
+        first.addAll(third);
+        return first;
     }
 }
